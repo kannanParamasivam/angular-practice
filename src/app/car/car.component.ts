2df9480aa71c1fab0fb09eeb4acc2c755b4aabdb
Kannan Paramasivam <kannan.paramasivam@live.com>
2018-07-02 17:36:23 -0500

    ng-for with index

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-car',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarComponent implements OnInit {
 
+  @Input() index: number = 0;
   rgbColor: string = "";
 
   constructor() {
